9c5d2dfe3cebf53f2d15ccc79a2c5b80667b8b2c
Timothy Sturm <timothysturm6@gmail.com>
2020-12-02 02:26:19 -0600

    2016 Day1 Part 2 completed

--- 2016/Day1/go/taxicab.go
+++ 2016/Day1/go/taxicab.go
@@ -24,8 +24,10 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		result := distance(line)
-		fmt.Printf("Total Distance (blocks): %v", result)
+		//result := distance(line)
+		//fmt.Printf("Total Distance (blocks): %v", result)
+		result := distance2(line)
+		fmt.Printf("First duplicate visit is %v blocks away", result)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -85,4 +87,71 @@ func distance(value string) int {
 	}
 
 	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+}
+
+type coord struct {
+	x int
+	y int
+}
+
+func distance2(value string) int {
+	var coords []coord
+	x := 0
+	y := 0
+	d := 0 // 0 = N, 1 = E, 2 = S, 3 = W, L = ++, R = --
+
+	entrys := strings.Split(value, ",")
+	for _, s := range entrys {
+		value := strings.TrimSpace(s)
+		dir := value[0]                               // first character
+		dis, err := strconv.Atoi(value[1:len(value)]) // remaining string
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if dir == 'L' {
+			if d == 3 {
+				d = 0
+			} else {
+				d++
+			}
+		}
+		if dir == 'R' {
+			if d == 0 {
+				d = 3
+			} else {
+				d--
+			}
+		}
+		for i := dis; i > 0; i-- {
+			if d == 0 {
+				x++
+			}
+			if d == 1 {
+				y++
+			}
+			if d == 2 {
+				x--
+			}
+			if d == 3 {
+				y--
+			}
+			c := coord{x, y}
+			if findPosition(c, coords) {
+				return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+			}
+			coords = append(coords, c)
+		}
+	}
+	log.Fatal()
+	return -1
+}
+
+func findPosition(c coord, x []coord) bool {
+	for _, v := range x {
+		if v.x == c.x && v.y == c.y {
+			return true
+		}
+	}
+	return false
 }
