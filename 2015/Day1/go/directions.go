f0a5153e2a22437ca0a62ea8bb36161c942f7b50
Timothy Sturm <timothysturm6@gmail.com>
2020-12-02 00:43:36 -0600

    2015 Day 1 Part 1, solution complete

---
+++ 2015/Day1/go/directions.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// First element in os.Args is always the program name,
+	// So we need at least 2 arguments to have a file name argument.
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name!")
+		return
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var value string
+	test := false
+	var expect int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if value == "" {
+			value = scanner.Text()
+		} else {
+			e, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Fatal(err)
+			}
+			expect = e
+			test = true
+		}
+	}
+
+	floors := countFloors(value)
+	fmt.Printf("Santa traveled to floor #%v\n", floors)
+	if test {
+		if floors == expect {
+			fmt.Println("Test passed!")
+		} else {
+			fmt.Println("Test Failed!")
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func countFloors(value string) int {
+	var result = 0
+	for _, c := range value {
+		if c == '(' {
+			result++
+		}
+		if c == ')' {
+			result--
+		}
+	}
+	return result
+}
