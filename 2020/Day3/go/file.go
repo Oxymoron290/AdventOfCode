8ee3b493ce21c8f71ae32ca6a7a3d0fa4c73f1ba
Timothy Sturm <timothysturm6@gmail.com>
2020-12-02 23:46:18 -0600

    2020 Day3 Part2 completed

--- 2020/Day3/go/file.go
+++ 2020/Day3/go/file.go
@@ -25,18 +25,27 @@ func main() {
 		line := scanner.Text()
 		mapFile = append(mapFile, line)
 	}
-	treeMap := buildMap(mapFile)
-	collisions := checkSlopeCollisions(treeMap, 1, 3)
 
-	fmt.Printf("Collision with %v trees", collisions)
+	treeMap1 := buildMap(mapFile, 1, 1)
+	collisions1 := checkSlopeCollisions(treeMap1, 1, 1)
+	treeMap2 := buildMap(mapFile, 1, 3)
+	collisions2 := checkSlopeCollisions(treeMap2, 1, 3)
+	treeMap3 := buildMap(mapFile, 1, 5)
+	collisions3 := checkSlopeCollisions(treeMap3, 1, 5)
+	treeMap4 := buildMap(mapFile, 1, 7)
+	collisions4 := checkSlopeCollisions(treeMap4, 1, 7)
+	treeMap5 := buildMap(mapFile, 2, 1)
+	collisions5 := checkSlopeCollisions(treeMap5, 2, 1)
+
+	fmt.Printf("Collision with %v trees", collisions1*collisions2*collisions3*collisions4*collisions5)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func buildMap(mapFile []string) [][]bool {
-	scale := len(mapFile) * 3
+func buildMap(mapFile []string, rise int, run int) [][]bool {
+	scale := (len(mapFile) / rise) * run
 	var treeMap [][]bool
 	for _, l := range mapFile {
 		var line []bool
@@ -67,7 +76,7 @@ func checkSlopeCollisions(treeMap [][]bool, rise int, run int) int {
 				continue
 			}
 			nextCol = j + run
-			fmt.Printf("At position %v row, %v col = %v\n", i, j, c)
+			//fmt.Printf("At position %v row, %v col = %v\n", i, j, c)
 			if c {
 				result++
 			}
