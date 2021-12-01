0db3c771d4bce6f8b7072e829e66d45a0aa70c48
Tim Sturm <tim.sturm@fairwaymc.com>
2021-12-01 14:06:41 -0600

    2021 - Day 1 - Puzzle 2; complete

---
+++ 2021/Day1/Puzzle2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+var input = System.IO.File.ReadAllLines("input.txt");
+
+var results = new List<int>();
+for (int i = 0; i < input.Count()-2; i++) {
+    var s1 = int.Parse(input[i]);
+    var s2 = int.Parse(input[i+1]);
+    var s3 = int.Parse(input[i+2]);
+    results.Add(s1+s2+s3);
+}
+
+int? previous = null;
+var count = 0;
+foreach (var level in results) {
+    if (previous != null) {
+        if (level > previous) {
+            count++;
+        }
+    }
+    previous = level;
+}
+
+Console.WriteLine("Total Increments: " + count);
