4fc5f7a36e08b9bee998047bbe3d1f41ee226726
Tim Sturm <tim.sturm@fairwaymc.com>
2021-12-01 13:21:25 -0600

    2021 - Day 1 - Puzzle 1; correct answer was 1451

---
+++ 2021/Day1/Puzzle1/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = System.IO.File.ReadAllLines("input.txt");
+
+int? previous = null;
+var count = 0;
+foreach (var line in input) {
+    var current = int.Parse(line);
+    if (previous != null) {
+        if (current > previous) {
+            count++;
+        }
+    }
+    previous = current;
+}
+
+Console.WriteLine("Total Increments: " + count);
