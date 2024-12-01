36418472265eb88ae82e5b492d1fededf875b191
Timothy Sturm <timothysturm6@gmail.com>
2024-12-01 12:53:55 -0600

    2024 bb

--- 2021/Day1/Puzzle2/obj/Debug/net6.0/Puzzle2.AssemblyInfo.cs
+++ 2021/Day1/Puzzle2/obj/Debug/net6.0/Puzzle2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Puzzle2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02454643470606190b02cabb81e76179f96075c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Puzzle2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Puzzle2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2024/Day1/Puzzle1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var list1 = new List<int>();
+var list2 = new List<int>();
+var input = File.ReadAllLines("input.txt").ToList();
+foreach (var line in input)
+{
+    var lineParts = line.Split("  ");
+    list1.Add(int.Parse(lineParts[0]));
+    list2.Add(int.Parse(lineParts[1]));
+}
+
+list1.Sort();
+list2.Sort();
+
+var distances = new List<int>();
+for (var i = 0; i < list1.Count; i++)
+{
+    var dist = 0;
+    if (list1[i] > list2[i])
+    {
+        dist = list1[i] - list2[i];
+    }
+    if (list2[i] > list1[i])
+    {
+        dist = list2[i] - list1[i];
+    }
+    distances.Add(dist);
+}
+
+var total = 0;
+foreach (var dist in distances)
+{
+    total += dist;
+}
+
+Console.WriteLine(total);
